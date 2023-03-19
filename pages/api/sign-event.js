4bfbb3346b5195fbb818586b963d005984d54a0f
Jacob Spevak <107512577+captain-stacks@users.noreply.github.com>
2023-03-19 14:01:43 -0500

    Code

---
+++ pages/api/sign-event.js
@@ -0,0 +1,19 @@
+import { getPublicKey, getEventHash, signEvent } from 'nostr-tools'
+
+export default async function handler(req, res) {
+  const { recipient, badgeId, badgeName } = req.body
+  const key = process.env.API_KEY
+  const event = {
+    kind: 8,
+    pubkey: getPublicKey(key),
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [
+      ["a", `30009:${badgeId}:${badgeName}`],
+      ["p", recipient]],
+    content: ''
+  }
+  event.id = getEventHash(event)
+  event.sig = signEvent(event, key)
+  
+  res.status(200).json(event)
+}
